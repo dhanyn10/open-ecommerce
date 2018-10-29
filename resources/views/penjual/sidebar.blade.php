e564b02263c0853b9360df803323ecbd2a8bed91
dhanyn10 <dhanyn.dhan@gmail.com>
2018-10-29 23:20:28 +0700

    change to accordion

--- resources/views/penjual/sidebar.blade.php
+++ resources/views/penjual/sidebar.blade.php
@@ -1,23 +1,35 @@
-<div class="col-md-3">
-    <div class="card">
-        <nav class="nav flex-column">
-            @if(Route::currentRouteName() == 'dasbor-penjual')
-                <a class="nav-link text-white bg-primary" href="{{route('dasbor-penjual')}}">Dasbor</a>
-            @else
-                <a class="nav-link text-dark" href="{{route('dasbor-penjual')}}">Dasbor</a>
-            @endif
-            
-            @if(Route::currentRouteName() == 'barang-penjual')
-                <a class="nav-link text-white bg-primary" href="{{route('barang-penjual')}}">Barang</a>
-            @else
-                <a class="nav-link text-dark" href="{{route('barang-penjual')}}">Barang</a>
-            @endif
-            
-            @if(Route::currentRouteName() == 'akun-penjual')
-                <a class="nav-link text-white bg-primary" href="{{route('akun-penjual')}}">Akun</a>
-            @else
-                <a class="nav-link text-dark" href="{{route('akun-penjual')}}">Akun</a>
-            @endif
-        </nav>
+<div class="col-md-3 sidebar">
+    <div id="accordion">
+        <div class="card">
+            <nav class="nav flex-column">
+                @if(Route::currentRouteName() == 'dasbor-penjual')
+                    <a class="nav-link text-white bg-primary" href="{{route('dasbor-penjual')}}">Dasbor</a>
+                @else
+                    <a class="nav-link text-dark" href="{{route('dasbor-penjual')}}">Dasbor</a>
+                @endif
+                
+                @if(Route::currentRouteName() == 'barang-penjual')
+                    <a class="nav-link text-white bg-primary collapsed" data-toggle="collapse" href="#barang">
+                        Barang
+                    </a>
+                    <div id="barang" class="collapse show" data-parent="#accordion">
+                        <a class="nav-link text-white bg-primary" href="{{route('barang-penjual')}}">Lihat Barang</a>
+                    </div>
+                @else
+                    <a class="nav-link text-dark collapsed" data-toggle="collapse" href="#barang">
+                        Barang
+                    </a>
+                    <div id="barang" class="collapse" data-parent="#accordion">
+                        <a class="nav-link text-dark" href="{{route('barang-penjual')}}">Lihat Barang</a>
+                    </div>
+                @endif
+
+                @if(Route::currentRouteName() == 'akun-penjual')
+                    <a class="nav-link text-white bg-primary" href="{{route('akun-penjual')}}">Akun</a>
+                @else
+                    <a class="nav-link text-dark" href="{{route('akun-penjual')}}">Akun</a>
+                @endif
+            </nav>
+        </div>
     </div>
 </div>
