f7833584ab15e0c5bdadfea87c1ac7d383aa05ba
dhanyn10 <dhanyn.dhan@gmail.com>
2018-10-29 22:33:56 +0700

    update form

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -1,9 +1,9 @@
 @extends('tataletak')
 @section('konten')
-<div class="col-md-4 tengah">
+<div class="col-md-4 offset-md-4">
     @include('flash::message')
     <div class="card border-primary w-100">
-        <div class="card-header bg-primary text-center text-white">Daftar akun baru</div>
+        <div class="card-header bg-primary text-center text-white">Daftar</div>
         <div class="card-body">
             <form method="post">
                 {{csrf_field()}}
