ee1ad4f7f166f718298adda90718d1a2963daa05
dhanyn10 <dhanyn.dhan@gmail.com>
2018-11-16 15:46:17 +0700

    hapus cropper dan ubah fungsi

--- public/js/penjual/tambah.js
+++ public/js/penjual/tambah.js
@@ -5,45 +5,12 @@ function bacagambar(input)
         var reader = new FileReader();
         reader.onload = function(e)
         {
-            $("#foto-barang").attr('src', e.target.result);
+            $("#preview-gambar").attr('src', e.target.result);
+            $("#data-gambar").val(e.target.result);
         }
         reader.readAsDataURL(input.files[0]);
     }
 }
-function croppergambar()
-{
-    $("#foto-barang").cropper({
-        aspectRatio: 4/4,
-        zoomable:true,
-        autoCropArea: 1.0,
-        cropBoxResizable:true,
-        crop: function(e) {
-        }
-    });
-    data_url =  $("#foto-barang").cropper('getCroppedCanvas',{
-        height:150,
-        width:150
-    }).toDataURL();
-    $("#input-barang").val(data_url);
-}
-$("#cekbarang").change(function(){
-    if($(".cropper-container").length)
-        {
-            $("#form-barang").html('<img name="foto-barang" id="foto-barang"/>');
-        }
+$("#gambar").change(function(){
     bacagambar(this);
-    setTimeout(function(){
-        croppergambar();
-    },100);
-    $("#crop").css("display","block");
-});
-$("#crop").click(function(){
-    barang = $("#foto-barang").cropper('getCroppedCanvas',{
-        height:150,
-        width:150
-    }).toDataURL();
-    $("#form-barang").html('<img name="foto-barang" id="foto-barang" src="'+barang+'"/>');
-    $("input[name='input-barang']").eq(0).val(barang);
-    //hilangkan tombol potong
-    $("#crop").css("display", "none");
 });
