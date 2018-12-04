44c6f50099ec69cb5598dba975ba7d7e5cdd5ad0
dhanyn10 <dhanyn.dhan@gmail.com>
2018-12-04 09:32:18 +0700

    pembaruan kolom, tidak menggunakan hash

--- database/migrations/2017_11_25_105848_tabel_pengguna.php
+++ database/migrations/2017_11_25_105848_tabel_pengguna.php
@@ -13,10 +13,10 @@ class TabelPengguna extends Migration
             $table->string("nama", 20);
             $table->string('telepon', 12)->nullable();
             $table->string('alamat',255)->nullable();
-            $table->string("sandi",60);
+            $table->string("sandi",30);
             $table->integer("peran");
             $table->integer("konfirmasi");
-            $table->string("token", 40);
+            $table->string("token", 40)->nullable();
         });
     }
     public function down()
