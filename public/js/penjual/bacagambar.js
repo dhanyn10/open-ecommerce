0c1bf1de2e4f6a3c23d387d6db32237bb2ee3f20
dhanyn10 <dhanyn.dhan@gmail.com>
2018-12-06 06:56:27 +0700

    validasi ukuran file(gambar) sebelum diupload

--- public/js/penjual/bacagambar.js
+++ public/js/penjual/bacagambar.js
@@ -12,5 +12,19 @@ function bacagambar(input)
     }
 }
 $("#gambar").change(function(){
-    bacagambar(this);
+    if(GetFileSize(this) <= 500)
+    {
+        bacagambar(this);
+    }
+    else
+    {
+        alert('File terlalu besar');
+        $(this).val(null);
+    }
 });
+function GetFileSize(input) {
+    fsize_kb    = 0;
+    fsize       = input.files.size;
+    fsize_kb    = Math.round((fsize / 1024));
+    return fsize_kb;
+}
