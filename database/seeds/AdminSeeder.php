c7b2e573403d1aa3a2cf24a48f0a147d03f9d3f0
dhanyn10 <dhanyn.dhan@gmail.com>
2020-05-09 23:30:52 +0700

    buat seeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('pengguna')->insert([
+            'email' => 'admin@open_ecommerce',
+            'nama'  => 'admin',
+            'sandi' => 'admin',
+            'peran' => 1,
+            'konfirmasi' => 1
+        ]);
+    }
+}
