caf211ae09a3ac7265956d89a1d348717c944d97
dhanyn10 <dhanyn.dhan@gmail.com>
2021-07-23 08:34:32 +0700

    login test admin

---
+++ tests/Unit/AdminTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Illuminate\Support\Facades\Session;
+
+class AdminTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testLoginAdmin()
+    {
+        Session::start();
+        $response = $this->call('POST', '/masuk', [
+            'email' => 'admin@open_ecommerce',
+            'sandi' => 'admin',
+            '_token' => csrf_token()
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
