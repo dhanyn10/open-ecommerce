459598d6f27b74c47c6e33217c30486b454ae30e
dhanyn10 <dhanyn.dhan@gmail.com>
2021-07-23 18:28:58 +0700

    feature-testing: penjual & pembeli login test

---
+++ tests/Feature/Auth/LoginPembeliTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Session;
+
+use PembeliSeeder;
+
+class LoginPembeliTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testExample()
+    {
+        Session::start();
+        $this->seed(PembeliSeeder::class);
+        $response = $this->call('POST', '/masuk', [
+            'email' => 'pembeli@open_ecommerce',
+            'sandi' => 'pembeli',
+            '_token' => csrf_token()
+        ]);
+        $response->assertRedirect('/');
+    }
+}
