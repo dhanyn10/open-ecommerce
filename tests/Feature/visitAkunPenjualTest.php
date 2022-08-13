e272a19d3b3738232f5e2f4510edbdbd850c55ae
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-13 19:39:01 +0700

    update + remove unused faker

--- tests/Feature/visitAkunPenjualTest.php
+++ tests/Feature/visitAkunPenjualTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
 
@@ -38,6 +37,5 @@ class visitAkunPenjual extends TestCase
         $response->assertSeeText('Akun');
         //content
         $response->assertSeeText('email');
-        $response->assertSeeText('nama');
     }
 }
