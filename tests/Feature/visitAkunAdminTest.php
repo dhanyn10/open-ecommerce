302ad53b1d9f7c51d1a52a242373c80a47f0a512
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-13 19:46:32 +0700

    admin test

---
+++ tests/Feature/visitAkunAdminTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Session;
+use Tests\TestCase;
+
+use AdminSeeder;
+
+class visitAkunAdminTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testLoginAdmin()
+    {
+        Session::start();
+        $this->seed(AdminSeeder::class);
+        $response = $this->call('POST', '/masuk', [
+            'email' => 'admin@open_ecommerce',
+            'sandi' => 'admin',
+            '_token' => csrf_token()
+        ]);
+        $response->assertRedirect(route('admin-dasbor'));
+        $response = $this->get('/admin/dasbor');
+        $response->assertStatus(200);
+        $response->assertSeeText('Profil');
+    }
+    
+    public function testVisitAkun()
+    {
+        $this->testLoginAdmin();
+        $response = $this->get('/admin/profil');
+        $response->assertStatus(200);
+        //content
+        $response->assertSeeText('profil');
+    }
+}
